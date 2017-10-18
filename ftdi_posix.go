897fc4a936f27064100aacde7781b387240070bf
Clement JACOB <clems71@gmail.com>
2017-10-18 16:47:57 +0200

    Update ftdi_posix.go

--- ftdi_posix.go
+++ ftdi_posix.go
@@ -3,7 +3,7 @@
 package ftdi
 
 /*
-#cgo CFLAGS: -I /usr/include/libftdi1 -I /usr/local/include/libusb-1.0
-#cgo LDFLAGS: -lftdi1 -lusb-1.0
+#cgo CFLAGS: -I /usr/include/libftdi1 -I /usr/local/include/libusb-1.0 -I /usr/local/include/libftdi1
+#cgo LDFLAGS: -lftdi1 -lusb-1.0 -L/usr/local/lib
 */
 import "C"
